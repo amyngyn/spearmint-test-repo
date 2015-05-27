80f2e2b43cd2294d37499bca2960deaf7d5d0345
amyngyn <amynguyen93@gmail.com>
2015-05-26 20:25:00 -0700

    added js and ruby files

---
+++ job.rb
@@ -0,0 +1,28 @@
+module TestRunner
+  class Job
+    
+    attr_writer :issues
+
+    def sources
+      raise 'abstract'
+    end
+    
+    def execute!(sources)
+      raise 'abstract'      
+    end
+
+    def issues
+      @issues or raise 'Job not yet run'
+    end
+
+    def success?
+      issues.none? { |issue| issue.fatal? }
+    end
+
+    class << self
+      def relative_filename(build, full_filename)
+        full_filename.sub(/#{Rails.root.join('clients', build.project.full_name, "#{build.commit}/")}/, '')
+      end
+    end
+  end
+end
